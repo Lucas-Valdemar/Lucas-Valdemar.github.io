3752afc2372a50125054d9469e8d70d1015c2468
Lucas Valdemar <lucas_valdemar@outlook.com>
2023-10-16 15:59:59 -0300

    Meu site mobile,  sem portfolio

--- scripts.js
+++ scripts.js
@@ -1,4 +1,3 @@
-console.clear();
 document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
   anchor.addEventListener("click", function (e) {
     e.preventDefault();
@@ -8,3 +7,134 @@ document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
     });
   });
 });
+
+class ImgSelector {
+  constructor(navigationName, imgName) {
+    this.navigationName = navigationName;
+    this.imgName = imgName;
+  }
+  getTabName(tabID) {
+    if (tabID === "portNavItem1") {
+      tabNumber = 1;
+    }
+    if (tabID === "portNavItem2") {
+      tabNumber = 2;
+    }
+    if (tabID === "portNavItem3") {
+      tabNumber = 3;
+    }
+    if (tabID === "portNavItem4") {
+      tabNumber = 4;
+    }
+  }
+  updateTabColor() {
+    if (tabNumber == 1) {
+      tab1.style.color = "#ffa500";
+      tab2.style.color = "#ffffff";
+      tab3.style.color = "#ffffff";
+      tab4.style.color = "#ffffff";
+    }
+    if (tabNumber == 2) {
+      tab2.style.color = "#ffa500";
+      tab1.style.color = "#ffffff";
+      tab3.style.color = "#ffffff";
+      tab4.style.color = "#ffffff";
+    }
+    if (tabNumber == 3) {
+      tab3.style.color = "#ffa500";
+      tab2.style.color = "#ffffff";
+      tab1.style.color = "#ffffff";
+      tab4.style.color = "#ffffff";
+    }
+    if (tabNumber == 4) {
+      tab4.style.color = "#ffa500";
+      tab2.style.color = "#ffffff";
+      tab3.style.color = "#ffffff";
+      tab1.style.color = "#ffffff";
+    }
+  }
+  updateImg() {
+    img1.remove();
+    img2.remove();
+    img3.remove();
+    if (tabNumber == 1) {
+      imgParent.appendChild(img1);
+      imgParent.appendChild(img2);
+      imgParent.appendChild(img3);
+      imgParent.style.width = "320px";
+      item1.style.width = "96px";
+      item2.style.width = "96px";
+      item3.style.width = "96px";
+      prog1.style.width = "96px";
+      prog2.style.width = "96px";
+      prog3.style.width = "96px";
+      prog1.style.top = "-25px";
+      prog2.style.top = "-25px";
+      prog3.style.top = "-25px";
+    }
+    if (tabNumber == 2) {
+      img2.remove();
+      img3.remove();
+      imgParent.appendChild(img1);
+      imgParent.style.width = "200px";
+      item1.style.width = "200px";
+      prog1.style.width = "200px";
+      prog1.style.top = "-50px";
+    }
+    if (tabNumber == 3) {
+      img1.remove();
+      img3.remove();
+      imgParent.appendChild(img2);
+      imgParent.style.width = "200px";
+      item2.style.width = "200px";
+      prog2.style.width = "200px";
+      prog2.style.top = "-50px";
+    }
+    if (tabNumber == 4) {
+      img1.remove();
+      img2.remove();
+      imgParent.appendChild(img3);
+      imgParent.style.width = "200px";
+      item3.style.width = "200px";
+      prog3.style.width = "200px";
+      prog3.style.top = "-50px";
+    }
+  }
+}
+
+const buttons = document.querySelectorAll(".button");
+const imgName = document.querySelectorAll(".imgLinks");
+const navigationName = document.querySelectorAll(".portNavItem");
+
+const imgParent = document.querySelector("#portImgViewer");
+const tab1 = document.querySelector("#portNavItem1");
+const tab2 = document.querySelector("#portNavItem2");
+const tab3 = document.querySelector("#portNavItem3");
+const tab4 = document.querySelector("#portNavItem4");
+const img1 = document.querySelector("#portImg1");
+const img2 = document.querySelector("#portImg2");
+const img3 = document.querySelector("#portImg3");
+const item1 = document.querySelector("#portfolioItem3");
+const item2 = document.querySelector("#portfolioItem4");
+const item3 = document.querySelector("#portfolioItem5");
+const prog1 = document.querySelector("#progress1");
+const prog2 = document.querySelector("#progress2");
+const prog3 = document.querySelector("#progress3");
+
+tabNumber = 1;
+
+const img = new ImgSelector(navigationName, imgName);
+
+buttons.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    const value = e.target.id;
+    console.log(value);
+    img.getTabName(value);
+    img.updateTabColor();
+    img.updateImg();
+  });
+});
+
+img.updateTabColor();
+img.updateImg();
+console.log(imgName);
